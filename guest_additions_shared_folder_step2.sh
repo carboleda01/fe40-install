b3bf269769111fe01e57edb4a72f202ffd3d93e9
carboleda01 <car_boleda@hotmail.com>
2023-01-12 16:47:57 +0100

    Update guest_additions_shared_folder_step2.sh

--- guest_additions_shared_folder_step2.sh
+++ guest_additions_shared_folder_step2.sh
@@ -1,5 +1,5 @@
 # mount shared folder
-username=car-b
+username="car-b"
 programName=shared
 mkdir ~/$programName
 sudo sh -c 'echo shared	/home/$username/$programName	vboxsf	defaults	0	0 >> /etc/fstab'

f080b0283acbb1806f33d129770b887afc9f0a80
carboleda01 <car_boleda@hotmail.com>
2023-01-12 16:18:12 +0100

    Create guest_additions_shared_folder_step2.sh

---
+++ guest_additions_shared_folder_step2.sh
@@ -0,0 +1,9 @@
+# mount shared folder
+username=car-b
+programName=shared
+mkdir ~/$programName
+sudo sh -c 'echo shared	/home/$username/$programName	vboxsf	defaults	0	0 >> /etc/fstab'
+sudo sh -c 'echo vboxsf >> /etc/modules'
+sudo mount -t vboxsf $programName ~/$programName
+
+sudo shutdown now
